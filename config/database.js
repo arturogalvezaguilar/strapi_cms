b48981cae993b7ea19cde83c4eac348760b0fa64
arturo.galvez <arturo.galvez@la-mark.com>
2020-08-21 16:18:19 -0500

    cambios

--- config/database.js
+++ config/database.js
@@ -13,3 +13,25 @@ module.exports = ({ env }) => ({
     },
   },
 });
+/*
+ module.exports = ({ env }) => ({
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "bookshelf",
+      "settings": {
+        "client": "mysql",
+        "host": "cicd.clyd3qcv4a9d.us-east-1.rds.amazonaws.com",
+        "port": "3306",
+        "database": "strapi_2",
+        "username": "MS_EVENT_USER_CI",
+        "password": "lamark"
+      },
+      "options": {
+        "ssl": false
+      }
+    }
+  }
+});
+*/
+
